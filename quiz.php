220169ca39a8da472d7cc9d9dec499d63645a967
gourav33310 <gourav33310@gmail.com>
2021-05-12 22:53:44 +0530

    commitex add.html

--- quiz.php
+++ quiz.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=\, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
+  <p id="demo" style="font-size:20px; color:orange;"></p>
+  <span id="seondspan"></span>
 <?php
 include "connection.php";
-
-echo "Today is " . date("l");
-echo "The time is " . date("sa");
 $select = "SELECT * FROM quizoption";
 $result = mysqli_query($con,$select);
 $i=1;
@@ -60,4 +60,49 @@ include "connection.php";
 ?>
     
 </body>
+<script>
+//       var seconds = Date.getSeconds();
+//         $("#seondspan").append(seconds);
+//  var countDownDate = new Date("May 10, 2021 18:50:25").getTime();
+
+// // Update the count down every 1 second
+// var x = setInterval(function() {
+
+//   // Get today's date and time
+//   var now = new Date().getTime();
+
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
+
+//   // Time calculations for days, hours, minutes and seconds
+// //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+// //   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// //   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   // Display the result in the element with id="demo"
+//   document.getElementById("demo").innerHTML = seconds + "s ";
+// //   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+// //   + minutes + "m " + seconds + "s ";
+
+
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
+var totaltime  =  30;   
+$("#seondspan").hide();
+setInterval(function(){ 
+     
+  totaltime--;
+    $("#seondspan").append(totaltime);
+    
+ }, 3000);
+
+     
+         
+    </script>
 </html>
+
